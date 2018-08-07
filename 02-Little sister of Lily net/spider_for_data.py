8e1329cafd526ac2f75a3682bf8b5eb015aa7389
Damon <33386246+Damon0626@users.noreply.github.com>
2018-08-07 23:57:45 +0800

    Update spider_for_data.py

--- 02-Little sister of Lily net/spider_for_data.py
+++ 02-Little sister of Lily net/spider_for_data.py
@@ -15,7 +15,7 @@ d = path.dirname(__file__)
 lover = imread(path.join(d, 'alice_color.png'))
 
 headers = {
-	"Cookie": "accessID=20180802221815489960; cookie_pcc=701%7C%7Cwww.baidu.com%7C%7C%7C%7Chttps%3A//www.baidu.com/link/url%3DStgFObDWACLrQlCvfhBxihGNHYC9T3lrTc-ISdoP57O%26ck%3D1978.1.62.318.155.314.141.4333%26shh%3Dwww.baidu.com%26sht%3Dbaiduhome_pg%26wd%3D%26eqid%3Dd152bbb300003625000000045b63129d; tempID=7430290578; NTKF_T2D_CLIENTID=guest7D336862-8DE5-84B9-E134-FB00E55EDF84; orderSource=10130301; lastLoginDate=Thu%20Aug%2002%202018%2023%3A09%3A44%20GMT+0800%20%28%u4E2D%u56FD%u6807%u51C6%u65F6%u95F4%29; accessToken=BH1533222585448369776; Hm_lvt_5caa30e0c191a1c525d4a6487bf45a9d=1533221743,1533221777,1533222109,1533222586; nTalk_CACHE_DATA={uid:kf_9847_ISME9754_guest7D336862-8DE5-84,tid:1533222586073023}; _fmdata=BJasv9VNXfN6fjLGGLSoX8jchsyL9uW%2BOVzKd28YiwXUjSlX6OwqP8zGLoxbfxOKIoTcKsTsFZhQ2U%2FCubVhRAWFOfcqF9xXEIIvd1J0sJU%3D; Hm_lpvt_5caa30e0c191a1c525d4a6487bf45a9d=1533222766",
+	"Cookie": "******", # yourself
 	"Host": "search.baihe.com",
 	"Origin": "http://search.baihe.com",
 	"Referer": "http://search.baihe.com/"
